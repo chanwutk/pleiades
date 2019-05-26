11edfd0ccdb043ab7097436291cb6957f0f141fc
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2019-05-26 00:54:33 -0700

    Already converted

--- src/components/TooltipTable.tsx
+++ src/components/TooltipTable.tsx
@@ -10,7 +10,7 @@ export const TooltipTable: React.FC<ITooltipTableProps> = ({
   id,
   children
 }) => (
-  <ReactTooltip effect="solid" id={id + ''}>
+  <ReactTooltip effect="solid" id={id}>
     <table className="tooltip-table">
       {children.map(([key, val]) => (
         <tr>
