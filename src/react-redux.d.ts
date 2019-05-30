a7683e9dbcc99cbe2a47d97bc06995b27b5bda08
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2019-05-30 14:55:59 -0700

    Don't pollute ns

--- src/react-redux.d.ts
+++ src/react-redux.d.ts
@@ -1,13 +1,11 @@
 import * as reactRedux from 'react-redux';
 import { Action, Dispatch, AnyAction } from 'redux';
 
-type UseSelector = <TState, TSelected>(
-  selector: (state: TState) => TSelected,
-  equalityFn?: (left: TSelected, right: TSelected) => boolean
-) => TSelected;
-
 declare module 'react-redux' {
   function useDispatch<A extends Action = AnyAction>(): Dispatch<A>;
-  const useSelector: UseSelector;
+  function useSelector<TState, TSelected>(
+    selector: (state: TState) => TSelected,
+    equalityFn?: (left: TSelected, right: TSelected) => boolean
+  ): TSelected;
   function shallowEqual<T>(left: T, right: T): boolean;
 }
