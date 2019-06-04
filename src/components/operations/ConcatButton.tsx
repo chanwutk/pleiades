a31c2d5bbe62258aca6c1bbcf540f99b6d942e36
Chanwut Kittivorawong <chanwutk@gmail.com>
2019-06-03 22:09:16 -0700

    concat button

--- src/components/operations/ConcatButton.tsx
+++ src/components/operations/ConcatButton.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Button from '@material-ui/core/Button';
 import { operateFactory } from './Utils';
 import { IOperationProps } from '../OperationBar';
+import { Menu, MenuItem } from '@material-ui/core';
 
 export const ConcatButton: React.FC<IOperationProps> = ({
   navBarOperands,
@@ -10,16 +11,63 @@ export const ConcatButton: React.FC<IOperationProps> = ({
 }) => {
   const operands = useSelector((state: IGlobalState) => state.current.operands);
   const operate = operateFactory(useDispatch(), operands);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClickConcat = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleSelectOption = (
+    option: InsertOrder,
+    orient: ConcatOrient
+  ) => () => {
+    handleMenuClose();
+    operate('concat', { option, orient });
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
 
   const concatDisabled =
     mainViewOperands.length !== 1 || navBarOperands.length !== 1;
 
   return (
-    <Button
-      onClick={() => operate('concat', { option: 'append', orient: 'h' })}
-      disabled={concatDisabled}
-    >
-      Concat
-    </Button>
+    <>
+      <Button
+        onClick={handleClickConcat}
+        disabled={concatDisabled}
+        aria-owns={anchorEl ? 'layer-menu' : undefined}
+        aria-haspopup="true"
+      >
+        Concat
+      </Button>
+
+      <Menu
+        id="layer-menu"
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleMenuClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        <MenuItem onClick={handleSelectOption('prepend', 'h')}>
+          On Left
+        </MenuItem>
+        <MenuItem onClick={handleSelectOption('append', 'h')}>
+          On Right
+        </MenuItem>
+        <MenuItem onClick={handleSelectOption('prepend', 'v')}>On Top</MenuItem>
+        <MenuItem onClick={handleSelectOption('append', 'v')}>
+          On Bottom
+        </MenuItem>
+      </Menu>
+    </>
   );
 };
