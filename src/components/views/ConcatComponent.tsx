9382197ea7ee49c911dd604b037985f24c390e95
Manesh Jhawar <manjhawar96@gmail.com>
2019-06-04 18:01:44 -0700

    Add init logic for concat

--- src/components/views/ConcatComponent.tsx
+++ src/components/views/ConcatComponent.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { IViewComponentProps, makeViewComponent } from './ViewComponent';
 import { VegaLite } from '../VegaLite';
+import { render } from './Renderer'; 
+import { ConcatView } from '../../SyntaxTree/ConcatView';
 
 export const ConcatComponent: React.FC<IViewComponentProps> = makeViewComponent(
-  ({ view }) => {
-    return <VegaLite spec={view.export()} />;
+  ({ view }) => { 
+    let subViews: any = (view as ConcatView).getSubViews();
+    
+    subViews.forEach((v) => {
+      render(v); 
+    });
   }
 );
