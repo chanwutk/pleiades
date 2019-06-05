5f0e9c5df8835d8b5e59a3d30b1caaa12947e1f3
Chanwut Kittivorawong <chanwutk@gmail.com>
2019-06-04 19:52:34 -0700

    complete concat

--- src/components/views/ConcatComponent.tsx
+++ src/components/views/ConcatComponent.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { IViewComponentProps, makeViewComponent } from './ViewComponent';
-import { VegaLite } from '../VegaLite';
-import { render } from './Renderer'; 
+import { render } from './Renderer';
 import { ConcatView } from '../../SyntaxTree/ConcatView';
 
 export const ConcatComponent: React.FC<IViewComponentProps> = makeViewComponent(
-  ({ view }) => { 
-    let subViews: any = (view as ConcatView).getSubViews();
-    
-    subViews.forEach((v) => {
-      render(v); 
-    });
+  ({ view, setIsHovering }) => {
+    const concatView = view as ConcatView;
+    const subViews = concatView.getSubViews();
+
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: concatView.getOrient() === 'h' ? 'row' : 'column',
+        }}
+      >
+        {subViews.map(subView => render(subView, setIsHovering))}
+      </div>
+    );
   }
 );
